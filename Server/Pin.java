9b43e5c784079c61f10c531e87785b1c11ab35cc
Jason <148025422+jayfvh@users.noreply.github.com>
2026-02-06 13:12:10 -0500

    fixing all mistakes, rolling back

--- Server/Pin.java
+++ Server/Pin.java
@@ -1,10 +1,3 @@
-client-gui
-
 //Base level class for storing data related to pin objects
 
 public record Pin(int x, int y) {}
-
-//Base level class for storing data related to pin objects
-
-public record Pin(int x, int y) {}
-main
